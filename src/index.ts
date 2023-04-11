e8d025a808dfcafb7c1dbdfd083f89c7de7db8e3
Lucas Simão <siimaoo.lucas@gmail.com>
2023-04-11 11:23:58 -0300

    fix: export functions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './close-cluster';
 export * from './get-cluster-at-current-zoom';
 export * from './get-spiderfied-cluster';
 export * from './open-cluster';
+export * from './find-marker';
+export * from './is-cluster';
+export * from './is-marker';
